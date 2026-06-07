ffa0d4697103527003ea36e4c5cc63d536318534
Ole Lenz <ole.lenz@gmx.net>
2026-06-07 15:37:35 +0200

    write stats to file

--- nes-common/src/Util/Statistics/NesStatistics.cpp
+++ nes-common/src/Util/Statistics/NesStatistics.cpp
@@ -1,5 +1,16 @@
 #include <Util/Statistics/NesStatistics.hpp>
+#include <fstream>
+
+// TODO: how do we delete stuff from the file?
 
 void NesStatistics::nesStats(const std::string& msg){
     std::cout << "NES-STAT: " << msg << std::endl;
+    std::lock_guard<std::mutex> lock(this->statsMutex);
+    std::ofstream outFile("stats-test.csv", std::ios::app);
+    if (!outFile.is_open()) {
+        std::cout << "Could not open file \n";
+        return;
+    }
+    outFile << msg << "\n";
+    outFile.close();
 }
